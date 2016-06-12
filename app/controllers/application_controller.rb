6e11d659a5742c7c767c6f05c0bbedea15b42271
msmukesh4 <msmukesh4@gmail.com>
2016-06-13 00:46:36 +0530

    implimention doorkeeper incomplete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  helper_method :gen_uuid, :encrypt, :decrypt
+  helper_method :gen_uuid, :encrypt, :decrypt, :current_user
 
   ALGORITHM = 'AES-128-ECB'
   KEY = "abckey123ccaabb1" # must me >= 16
+
+  	def current_user
+  		@current_user ||= User.find(session[:user_id]) if session[:user_id]
+	end
 
   # this will ecript the data based on the key and Algorithm type
   def encrypt(data)
