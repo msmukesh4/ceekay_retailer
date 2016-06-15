802a5fcc77bd243b9835c897e5de757cc14c6f8f
msmukesh4 <msmukesh4@gmail.com>
2016-06-15 20:39:20 +0530

    able to upload excel file locally

--- app/controllers/upload_controller.rb
+++ app/controllers/upload_controller.rb
@@ -6,7 +6,7 @@ class UploadController < ApplicationController
 	def export_xls_to_db(path)
 		row_number = -1
 		@user_count = 0
-		workbook = RubyXL::Parser.parse("/home/mukesh/Desktop/ck_retailers.xlsx")
+		workbook = RubyXL::Parser.parse("#{Rails.public_path}/uploads/ck_retailers.xlsx")
 		worksheet = workbook[0]
 		worksheet.each { |row|
 		  	
