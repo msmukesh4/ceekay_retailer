b9b46a7d59f2849b18001afccc3cf69cdebd881e
msmukesh4 <msmukesh4@gmail.com>
2016-06-21 09:04:41 +0530

    header-footer ui changes

--- app/models/retailer.rb
+++ app/models/retailer.rb
@@ -6,7 +6,7 @@ class Retailer < ActiveRecord::Base
 	def self.search(dse_code,route,retailer_name,retailer_code)
 	  if dse_code or route or retailer_name or retailer_code
 	  	puts "dse = #{dse_code}, route = #{route} , retailer_name = #{retailer_name}, retailer_code = #{retailer_code}"
-	    find(:all, :conditions => ['dse_code LIKE ? AND route_no LIKE ? AND retailer_name LIKE ? AND retailer_code LIKE ?', "%#{dse_code}%", "%#{route}%", "%#{retailer_name}%", "%#{retailer_code}%"])
+	    find(:all, :conditions => ['dse_code ILIKE ? AND route_no ILIKE ? AND retailer_name ILIKE ? AND retailer_code ILIKE ?', "%#{dse_code}%", "%#{route}%", "%#{retailer_name}%", "%#{retailer_code}%"])
 	  else
 	    find(:all)
 	  end
